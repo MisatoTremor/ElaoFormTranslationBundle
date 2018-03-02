3684880535aca09064b6417fca99c747d31524e0
Steffen Roßkamp <steffen.rosskamp@gimmickmedia.de>
2018-03-02 11:16:34 +0100

    Add support for nested options using PropertyAccessor

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -118,6 +118,7 @@ class Configuration implements ConfigurationInterface
             ->prototype('scalar')
                 ->isRequired()
             ->end()
+            ->normalizeKeys(false)
             ->defaultValue($default);
 
         return $node;
